8e7bc14a818f86a1ed11bcb30d0244cae321bd65
urana03 <uhackninja@gmail.com>
2022-11-25 14:21:29 +0530

    Add: src Folder

---
+++ src/main/java/com/bridgelabz/generics/Welcome.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.generics;
+
+/**
+ * Hello world!
+ *
+ */
+public class Welcome 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Java Programs" );
+    }
+}
